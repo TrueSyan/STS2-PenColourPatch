102d9dd97ab25ad5980d390b46d4c7d074432a82
truesyan <truesyan@gmail.com>
2026-04-01 02:47:39 +1100

    Added PenColourPatch.cs

--- PenColourPatchCode/Patches/PenColourPatch.cs
+++ PenColourPatchCode/Patches/PenColourPatch.cs
@@ -1,6 +1,32 @@
-﻿namespace PenColourPatch.PenColourPatchCode.Patches;
+﻿using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Nodes.Screens.Map;
 
+namespace PenColourPatch.PenColourPatchCode.Patches;
+
+[HarmonyPatch(typeof(NMapDrawings), "CreateLineForPlayer")]
 public class PenColourPatch
 {
+    private static double id;
     
+    static void Prefix(Player player)
+    {
+        id = (double) player.NetId;
+        // MainFile.Logger.Info($"Player NetID: {player.NetId}.");
+    }
+    
+    static void Postfix(ref Line2D __result)
+    {
+        // Generate a small random colour deviation +/- the original value using the player id
+        float deviation = 0.2f;
+        
+        float red = (float) ((Math.Cos(id) - 0.5) * deviation);
+        float green = (float) ((Math.Sin(id) - 0.5) * deviation);
+        float blue = (float) ((((Math.Cos(id) + Math.Sin(id)) / 2) - 0.5) * deviation);
+        
+        MainFile.Logger.Info($"ID: {id}, Red: {red:F}, Green: {green:F}, Blue: {blue:F}");
+        
+        __result.DefaultColor += new Color(red, green, blue);
+    }
 }
